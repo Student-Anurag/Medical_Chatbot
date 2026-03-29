18dd707f6ebd6ae20a0bb9b775a76b3a8077fa7e
Anurag Chakraborty <anuragchakraborty00410041@gmail.com>
2026-03-29 12:07:24 +0530

    Requirements added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Medical_Chatbot
 This is an end to end project on LLM where I implemented a medical chatbot using Langchain
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Anurag Chakraborty',
+    author_email='anuragchakraborty00410041@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
